03b9af100a92762a6c9656c0586ae702484dd707
bsxxu <BsxHomu@outlook.com>
2024-09-08 19:29:22 +0800

    feat: something

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,14 +2,9 @@ import 'dotenv/config';
 import { serverConfig } from './configs/server.config';
 import { createServer } from './server/server';
 
-const server = createServer();
-
 (async () => {
+	const server = createServer();
 	try {
-		server.get('/test', (request, reply) => {
-			request.log.debug(process.env.TEST);
-			reply.send({ hello: 'world!!!' });
-		});
 		await server.listen({ port: serverConfig.port });
 	} catch (err) {
 		server.log.error(err);
