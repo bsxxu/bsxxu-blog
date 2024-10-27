350f3facb42cd3d3c183bc51d6df760aaea11f97
bsxxu <bsxhomu@outlook.com>
2024-10-27 21:09:31 +0800

    feat: home motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import BgHome from '@/components/common/bg-home';
+import DownToTopView from '@/components/motion/down-to-top-view';
 import Link from 'next/link';
 
 export default function Home() {
@@ -6,20 +7,26 @@ export default function Home() {
     <>
       <BgHome />
       <div className="max-w-2xl mx-auto mt-44 px-5">
-        <div className="font-semibold text-4xl">Hi! 👋, I&apos;m Bsx.</div>
-        <div className="mt-10">
+        <DownToTopView className="font-semibold text-4xl">
+          Hi! 👋, I&apos;m Bsx.
+        </DownToTopView>
+        <DownToTopView className="mt-10">
           I am a college student studying software engineering. I mainly use
           react for front-end development.
-        </div>
-        <div className="mt-6 font-semibold text-lg">about me</div>
-        <div className="mt-3">
+        </DownToTopView>
+        <DownToTopView className="mt-6 font-semibold text-lg">
+          about me
+        </DownToTopView>
+        <DownToTopView className="mt-3">
           I am a coder driven by interest, and I love learning new knowledge or
           skills. Besides programming, I like reading and watching documentaries
           in free time. I am also an anime fan, as you can see from my avatar,
           which was drew by myself.
-        </div>
-        <div className="mt-6 font-semibold text-lg">find me</div>
-        <div className="mt-3">
+        </DownToTopView>
+        <DownToTopView className="mt-6 font-semibold text-lg">
+          find me
+        </DownToTopView>
+        <DownToTopView className="mt-3">
           You can find me at&nbsp;
           <Link href="https://github.com/BsXwerse" className=" underline">
             GitHub
@@ -32,7 +39,7 @@ export default function Home() {
           <Link href="https://t.me/bsxxu" className=" underline">
             telegram
           </Link>
-        </div>
+        </DownToTopView>
       </div>
     </>
   );
--- src/components/post/article.tsx
+++ src/components/post/article.tsx
@@ -5,20 +5,14 @@ import Toc from './toc';
 
 export default async function Article({ content }: { content: string }) {
   return (
-    <div className="flex justify-center p-5 gap-5">
-      <motion.div
-        className="w-full"
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
-      >
-        <MDXRemote content={content} />
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0, x: 50 }}
-        animate={{ opacity: 1, x: 0 }}
-      >
-        <Toc headings={await getHeadings(content)} />
-      </motion.div>
-    </div>
+    <motion.div
+      className="flex justify-center p-5 gap-5"
+      initial={{ opacity: 0.5, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ ease: 'easeOut' }}
+    >
+      <MDXRemote content={content} />
+      <Toc headings={await getHeadings(content)} />
+    </motion.div>
   );
 }
